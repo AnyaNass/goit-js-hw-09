316be51377edb48744927d457cced6a015b956a6
Anna <98224289+AnyaNass@users.noreply.github.com>
2022-09-01 11:47:39 +0300

    color-checker is done

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,26 @@
 
+const refs = {
+	start: document.querySelector("[data-start]"),
+	stop: document.querySelector("[data-stop]"),
+	body: document.querySelector('body'),
+}
+let timeId = null;
+
+refs.start.addEventListener("click", onClickChangeBgd);
+refs.stop.addEventListener("click", onClickStopChangeBgd);
+
+function onClickChangeBgd() {
+	refs.start.setAttribute("disabled", true)
+	timeId = setInterval(() => {
+		refs.body.style.backgroundColor = getRandomHexColor();
+	}, 1000)
+}
+
+function getRandomHexColor() {
+	return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+function onClickStopChangeBgd() {
+	clearInterval(timeId);
+	refs.start.removeAttribute("disabled");
+}
