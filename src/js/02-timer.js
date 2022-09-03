10d5ee3e24606dcdcd9c2052c55a87d016b25437
Anna <98224289+AnyaNass@users.noreply.github.com>
2022-09-03 22:45:55 +0300

    add  flatpickr to timer

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,3 +1,16 @@
+import flatpickr from "flatpickr";
+import "flatpickr/dist/flatpickr.min.css";
+
+flatpickr("#datetime-picker", {
+	enableTime: true,
+	time_24hr: true,
+	defaultDate: new Date(),
+	minuteIncrement: 1,
+	onClose(selectedDates) {
+		console.log(selectedDates[0]);
+	},
+});
+
 const refs = {
 	date: document.querySelector('#datetime-picker'),
 	days: document.querySelector('[data-days]'),
@@ -13,11 +26,25 @@ let timeId = null;
 function timer() {
 	const timeBack = new Date(refs.date.value) - new Date();
 
-	console.log(timeBack);
-	refs.days.textContent = pad(Math.floor(timeBack / (1000 * 60 * 60 * 24) % 30))
-	refs.hours.textContent = pad(Math.floor((timeBack / (1000 * 60 * 60)) % 24))
-	refs.minutes.textContent = pad(Math.floor((timeBack / (1000 * 60)) % 60))
-	refs.seconds.textContent = pad(Math.floor((timeBack / 1000) % 60))
+	refs.days.textContent = addLeadingZero(convertMs(timeBack).days);
+	refs.hours.textContent = addLeadingZero(convertMs(timeBack).hours);
+	refs.minutes.textContent = addLeadingZero(convertMs(timeBack).minutes);
+	refs.seconds.textContent = addLeadingZero(convertMs(timeBack).seconds);
+}
+
+function convertMs(ms) {
+	const second = 1000;
+	const minute = second * 60;
+	const hour = minute * 60;
+	const day = hour * 24;
+
+
+	const days = Math.floor(ms / day);
+	const hours = Math.floor((ms % day) / hour);
+	const minutes = Math.floor(((ms % day) % hour) / minute);
+	const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+	return { days, hours, minutes, seconds };
 }
 
 refs.date.addEventListener("change", () => {
@@ -29,12 +56,15 @@ refs.date.addEventListener("change", () => {
 		refs.seconds.textContent = "00"
 		refs.tooltip.classList.remove('hidden')
 	}
+
 	if (refs.date.value == "") {
 		refs.tooltip.classList.add('hidden')
 	}
+
 	if (new Date(refs.date.value) > new Date) {
 		refs.start.removeAttribute("disabled")
 	}
+
 	clearInterval(timeId);
 })
 
@@ -43,13 +73,15 @@ refs.start.addEventListener('click', () => {
 		refs.start.setAttribute("disabled", true);
 		return
 	}
+
 	refs.start.setAttribute("disabled", true);
 	refs.tooltip.classList.add('hidden')
+
 	timeId = setInterval(timer, 1000);
 });
 
 
-function pad(value) {
+function addLeadingZero(value) {
 	return String(value).padStart(2, '0');
 }
 
